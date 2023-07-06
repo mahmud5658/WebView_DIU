2100b2071bfd55cd7c360527ef9d5c7eb8d475fa
Abdullah <almahmuda5@gmail.com>
2023-07-06 22:28:16 +0600

    web view diu

---
+++ app/src/main/java/com/cyberx/webview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cyberx.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+
+        webView.loadUrl("https://daffodilvarsity.edu.bd/");
+
+        WebSettings webSettings = webView.getSettings();
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
